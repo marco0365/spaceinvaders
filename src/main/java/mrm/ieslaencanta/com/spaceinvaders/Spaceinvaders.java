2502c3fb9f3f6a85b88d72c2b20b4d26ef092e1c
Marco Examen <marco@examen.com>
2023-02-03 19:07:01 +0100

    Primer

---
+++ src/main/java/mrm/ieslaencanta/com/spaceinvaders/Spaceinvaders.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package mrm.ieslaencanta.com.spaceinvaders;
+
+/**
+ *
+ * @author DAWTarde
+ */
+public class Spaceinvaders {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
